7017cbdc820ba418c1e62e137d3037cc6343ddfb
saan1984 <sandeep_giet@yahoo.com>
2014-10-24 21:34:40 +0530

    Initial commit

---
+++ assets/script/deviceTypeProvider.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module("profileApp.deviceTypeProvider",[])
+
+.provider('deviceType', ['$windowProvider', function($windowProvider) {
+    var $window = $windowProvider.$get();
+    this.$get = function() {
+       return{
+           /*Returns the device type desktop, mobile and tablet, default device type is desktop*/
+           getDeviceType:function(){
+               var deviceType='desktop',
+                   userAgentString = $window['navigator']['userAgent'] ||
+                                     $window['navigator']['vendor'] ||
+                                     $window['opera'],
+                   width = $window['outerWidth'],
+                   isSmart = (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(userAgentString);
+                   if(isSmart&& width >= 768){
+                       deviceType = "tablet";
+                   }else if(isSmart&& width <= 767){
+                       deviceType = "mobile";
+                   }
+               return deviceType;
+           }
+       }
+    };
+}])
